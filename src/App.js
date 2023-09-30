abb72413255f9d0794fab76b9a9ba2b09271d487
pikigene <pikigene01@gmail.com>
2023-09-30 23:16:19 +0200

    update youtube app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppProvider from "./contexts/AppProvider";
 import Game from "./pages/Game";
+import YouTube from "./pages/YouTube";
 
 function App() {
   return (
     <Router>
       <AppProvider>
       <Routes>
-        <Route exact path="/" element={<Game />} />
+        <Route exact path="/" element={<YouTube />} />
       </Routes>
       </AppProvider>
     </Router>
--- src/pages/YouTube.jsx
+++ src/pages/YouTube.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import useLocalStorage from "../services/useLocalStorage";
 
 export default function YouTube() {
-  const [id, setId] = useLocalStorage("id", 0);
+ const [copiedText,setCopiedText] = useState("")
+ useEffect(()=>{
+  navigator.clipboard.readText().then((text)=>{
+    setCopiedText(text);
+  })
+ },[])
+
+ useEffect(()=>{
+var textWrap = document.getElementById('text')
+
+if(copiedText.length > 2){
+  let i = 0;
+  setInterval(()=>{
+   i++;
+   textWrap.innerText = copiedText.substring(0,i)
+   if(i >= copiedText.length){
+    textWrap.innerHTML = copiedText;
+   }
+  },2)
+}
+ },[copiedText])
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: 'center'
-      }}
-    >
-      <button onClick={(e) => setId(id+1)}>{id} PikiDelivery YouTube</button>
+    <div id="text">
+      <h1 style={{color:"#333"}}>PikiDelivery</h1>
     </div>
   );
 }
