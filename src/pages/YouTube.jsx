c2f172df56639c001cec907ae8ecf9fe9f91dc5d
pikigene <pikigene01@gmail.com>
2023-10-12 05:45:54 +0200

    update app

--- src/pages/YouTube.jsx
+++ src/pages/YouTube.jsx
@@ -2,8 +2,18 @@ import React, { useEffect,useState } from "react";
 import useLocalStorage from "../services/useLocalStorage";
 import { useSelector,useDispatch } from "react-redux";
 import { buttonClick } from "../redux/cartReducer";
+import { useParams, useSearchParams } from "react-router-dom";
 
 export default function YouTube() {
+  const [searchParams,setSearchParams] = useSearchParams({q:""});
+  const query = searchParams.get("q");
+
+  const hanldeParams =(e)=>{
+    setSearchParams(prev => {
+      prev.set("q", e.target.value);
+      return prev;
+    })
+  }
  const {count} = useSelector(state=>state.cart)
  const dispatch = useDispatch();
   const addCount =(e)=>{
@@ -12,6 +22,7 @@ export default function YouTube() {
   return (
     <div id="text">
       <h1 onClick={()=>addCount()} style={{color:"#333"}}>PikiDelivery {count}</h1>
+      <input type="text" value={query} onChange={hanldeParams}/>
     </div>
   );
 }
